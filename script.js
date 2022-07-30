b41a8619c569aa8592f96e4a2f561d00eefc0f3d
curiouspi2021 <83997537+curiouspi2021@users.noreply.github.com>
2022-07-31 01:40:11 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,25 +2,45 @@
 let search = document.getElementById("search")
 let searchResults = document.getElementById("searchResults")
 let searchButton = document.getElementById("searchButton")
+let videoId = document.getElementById("videoId")
+let playButton = document.getElementById("playButton")
+let youtubeAudio = document.getElementById("youtube-audio")
+
+function playAudio(){
+    // youtubeAudio.setAttribute("data-video", videoId.value) 
+    youtubeAudio.classList.remove("d-none")
+}
 
 function getData() {
-    let url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${search.value}&key=AIzaSyB_0eDhnTPdODs8D2PhQ6vWIPY7IILAAXc`
+    let url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=${search.value}&key=AIzaSyB_0eDhnTPdODs8D2PhQ6vWIPY7IILAAXc`
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
         searchResults.textContent = ""
         for(let item of data['items']){
-            let link = `https://www.youtube.com/embed/${item['id']['videoId']}`
+            let thumbnailUrl = item["snippet"]["thumbnails"]["default"]["url"]
             let videoBox = document.createElement("div")
-            videoBox.classList.add("col-12") 
-            videoBox.classList.add("col-md-4") 
+            videoBox.classList.add("d-flex") 
+            videoBox.classList.add("align-items-center") 
+            videoBox.classList.add("border") 
+            videoBox.classList.add("mx-2") 
             searchResults.appendChild(videoBox)
-            let videoIFrame = document.createElement("iframe")
-            videoIFrame.src = link 
-            videoBox.appendChild(videoIFrame)
+            let thumbnailImg = document.createElement("img")
+            thumbnailImg.src = thumbnailUrl 
+            videoBox.appendChild(thumbnailImg)
+            let contentBox = document.createElement("div")
+            contentBox.classList.add("mx-2")
+            videoBox.appendChild(contentBox)
+            let title = document.createElement("h5")
+            title.textContent = item["snippet"]["title"]
+            let id = document.createElement("p")
+            id.textContent = item['id']['videoId']
+            contentBox.appendChild(title)
+            contentBox.appendChild(id)
         }
       });
 }
 
 searchButton.addEventListener("click", getData)
+playButton.addEventListener("click", playAudio)
 
